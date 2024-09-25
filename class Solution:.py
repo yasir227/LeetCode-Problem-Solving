c332bdd917269a613dbf80aa593a7253b81e26c2
yasir227 <yasir.turzo@gmail.com>
2024-09-25 11:12:52 -0700

    Problem Solving

--- class Solution:.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        c = cost
-        c.append(0)
-
-        for i in range(len(c) - 3, -1, -1):
-            c[i] += min(c[i + 1], c[i + 2])
-        return min(c[0], c[1])
-        
